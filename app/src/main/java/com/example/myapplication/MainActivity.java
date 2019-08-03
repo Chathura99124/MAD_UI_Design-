d721f7a7521d8c047d48cc6723a3a457c848b09f
SMOKE <agpdinuka1998@gmail.com>
2019-08-03 22:40:32 +0530

    search_updated

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button srch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        srch = findViewById(R.id.srch);
+
+        srch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent redirect_srch = new Intent(MainActivity.this,search.class);
+                startActivity(redirect_srch);
+            }
+        });
+
     }
 }
