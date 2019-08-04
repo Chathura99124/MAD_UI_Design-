cd12b720789bcaec18cd9b9d975856ea4284b5b2
Chathura99124 <chathura749@gmail.com>
2019-08-04 17:21:15 +0530

    Update_Delete_Foods_V1

---
+++ app/src/main/java/com/example/myapplication/Update_Delete_Food.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Update_Delete_Food extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update__delete__food);
+    }
+}
