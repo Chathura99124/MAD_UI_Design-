fa0a34a0966ff90818075577dbc145875ccc71c7
shakith1 <nimsara.shakith@gmail.com>
2019-08-06 14:27:19 +0530

    Boarding Places

--- app/src/main/java/com/example/myapplication/boarding_places.java
+++ app/src/main/java/com/example/myapplication/boarding_places.java
@@ -1,14 +1,14 @@
 package com.example.myapplication;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class appoinment  extends AppCompatActivity {
+import android.os.Bundle;
+
+public class boarding_places extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_appoinment);
+        setContentView(R.layout.activity_boarding_places);
     }
 }
